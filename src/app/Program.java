72f017c3cbc144094c40ac95ace5d0e9f7e54398
Bernardo Braga <bebraga12@gmail.com>
2024-07-16 23:26:01 -0300

    Classe Seller

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,18 @@
 package app;
 
+import java.util.Date;
+
 import model.Department;
+import model.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         
         Department obj = new Department(1, "Books");
-        System.out.println(obj);        
+        System.out.println(obj);
+        
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 30000.0, obj);
+        System.out.println(seller);
 
     }
 }
